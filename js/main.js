e1b005b69c562e026a1d37b72a6cea73d5c96b1b
Ali <letslearntocode@mail.com>
2018-07-08 11:47:16 +0000

    updated the files

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
 var typed = new Typed('.blink', {
-  strings: ["Hello World!", " Let's Have Fun in Amsterdam Tonight!"],
-  typeSpeed: 90
+  strings: ["Hello World!", "Let's Have a Party in Amsterdam to Celebrate King's Day!:)"],
+  typeSpeed: 100
 });
